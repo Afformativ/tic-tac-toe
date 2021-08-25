ce8bff210824a9e41133065b3266e83431da3460
Afformativ <40630611+Afformativ@users.noreply.github.com>
2021-08-25 13:36:39 +0300

    Update tic-tac-toe.js

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,52 @@
 class TicTacToe {
     constructor() {
-
+        this.arr = [[null, null, null],[null, null, null],[null, null, null]];
+        this.x = true;
+        this.previous = '';
     }
 
-    getCurrentPlayerSymbol() {
-
+    getCurrentPlayerSymbol() { 
+        return this.x ? 'x' : 'o';
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (this.arr[columnIndex][rowIndex] === null){
+        this.arr[columnIndex][rowIndex] = this.getCurrentPlayerSymbol();
+        this.x = !this.x;
+        }
     }
 
     isFinished() {
-
+        if (this.isDraw()) return true;
+        return this.getWinner() ? true : false;
     }
 
     getWinner() {
-
+        for (let i = 0; i < this.arr.length; i++)
+        {
+            if (this.arr[i][0] === this.arr[i][1]  && this.arr[i][1] === this.arr[i][2]) return this.arr[i][0];
+            if (this.arr[0][i] === this.arr[1][i] && this.arr[1][i] === this.arr[2][i]) return this.arr[0][i]; 
+        }
+        if (this.arr[0][0] === this.arr[1][1] && this.arr[1][1] === this.arr[2][2]) return this.arr[0][0];
+        if (this.arr[0][2] === this.arr[1][1] &&  this.arr[1][1] === this.arr[2][0]) return this.arr[0][2];
+        return null;
     }
 
     noMoreTurns() {
-
+        for (let i = 0; i < this.arr.length; i++)
+        for (let j = 0; j < this.arr[i].length; j++)
+        if (this.arr[i][j] === null) return false;
+        return true;
     }
 
     isDraw() {
-
+        if (!this.noMoreTurns()) return false; 
+        if (this.getWinner() === 'x' || this.getWinner() === 'o') return false;
+        return true;
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.arr[colIndex][rowIndex];
     }
 }
 
